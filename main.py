406fddc50213c0a551bda8e1f27b04431f96903d
Vresod <lukebinkofsky@gmail.com>
2020-10-14 11:09:42 -0400

    initial commit based off of dicearolla and user-imitator

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import discord
+import random
+from discord.ext import commands
+
+with open("tokenfile","r") as tokenfile:
+	token = tokenfile.read()
+
+client = commands.Bot(command_prefix="m!")
+client.remove_command("help")
+
+@client.event
+async def on_ready(): 
+	print(f"logged in as {client.user}")
+	print(f"https://discord.com/oauth2/authorize?client_id={client.user.id}&permissions=0&scope=bot")
+	for guild in client.guilds:
+		print(f"In guild: {guild.name}") 
+
+@client.event
+async def on_guild_join(guild):
+	print(f"Joined guild: {guild.name}")
+
+@client.command() 
+async def minesweeper(ctx, length: int, width: int, mines: int):
+	await ctx.send("wip")
+
+
+@client.command()
+async def help(ctx):
+	await ctx.send("use d!roll {number of dice} {number of sides} to roll the bones")
+
+client.run(token)

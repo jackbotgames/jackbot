5d2b614c86601bdda04798f23e4ad3ab2b6158f7
Vresod <lukebinkofsky@gmail.com>
2020-11-09 12:12:35 -0500

    add m!roll, stolen from dicearolla whose roll function was stolen from kraken

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ async def on_guild_join(guild):
 async def minesweeper(ctx, length: int, width: int, mines: int):
 	await ctx.send("wip")
 
+@client.command()
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+	dice = [
+		str(random.choice(range(1, number_of_sides + 1)))
+		for _ in range(number_of_dice)
+	]
+	await ctx.send(', '.join(dice))
 
 @client.command()
 async def help(ctx):
